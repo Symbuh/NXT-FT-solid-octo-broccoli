45453ec248d4f963b9b819cecec28eb375b2031b
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2023-03-02 18:53:11 -0800

    Added single note component allowing you to click on a single note to go to it's page

---
+++ src/app/notes/[id]/page.tsx
@@ -0,0 +1,31 @@
+async function getNote(noteID: string) {
+  /*
+    Because this a 'dynamic route', it won't automatically cache every req
+
+    You can update the caching behavior by implementing static generation
+      The second parameter below ensures that this page is updated every
+      10 seconds.
+  */
+  const res = await fetch(
+    `http://http://127.0.0.1:8090/api/collections/notes/records/${noteID}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+  const data = await res.json();
+  return data;
+}
+export default async function NotePage({ params }: any) {
+  const note = await getNote(params.id);
+
+  return (
+    <div>
+      <h1>notes/{note.id}</h1>
+      <div className={styles.note}>
+        <h3>{note.title}</h3>
+        <h5>{note.content}</h5>
+        <p>{note.created}</p>
+      </div>
+    </div>
+  );
+}
